3cb83d519b33248f46ad7d842f73108e5d475a01
caoYong123456 <130946836+caoYong123456@users.noreply.github.com>
2024-03-15 15:18:33 +0800

    111

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 //export const url='http://localhost:8081/hgj';
 //export const url='http://192.168.79.5:82/mgr';
 //export const url='https://zhgj.shofw.com/mgr';
-export const url='http://192.168.23.108:82/mgr';
+//export const url='http://192.168.23.108:82/mgr';
 //export const url='http://192.168.79.133:82/mgr';
-//export const url='https://zhgj.xhguanjia.cn/mgr';
+export const url='https://zhgj.xhguanjia.cn/mgr';
 
